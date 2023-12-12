5327b9b4f4ba8a17a3e974e87daf3670e037ac34
Conartisttt <connerwmar@gmail.com>
2023-12-12 17:32:08 -0500

    comments added to project

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const inquirer = require('inquirer');
 const questions = require('./lib/questions');
 const write = require('./lib/writeFile');
 
+// launches inquirer prompts in terminal and passes responses to writeFile
 inquirer
   .prompt(questions)
   .then((response) =>
